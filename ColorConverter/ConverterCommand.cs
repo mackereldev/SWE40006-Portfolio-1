ae80d3b95f8cf856be5edad90e3b4a22dde1e72e
mackereldev <sabashaw.home@gmail.com>
2026-08-18 17:08:51 +1000

    feat: use ConsoleAppFramework to create a noninteractive CLI app

---
+++ ColorConverter/ConverterCommand.cs
@@ -0,0 +1,43 @@
+﻿namespace ColorConverter;
+
+using ConsoleAppFramework;
+
+public static class ConverterCommand {
+	public enum Form {
+		RGB,
+		HSL,
+		HSV
+	}
+
+	/// <summary>
+	/// Convert a hexadecimal color to another CSS-compatible form.
+	/// </summary>
+	/// <param name="color">The hexadecimal color to convert.</param>
+	/// <param name="form">-f, The color form to convert to (one of RGB, HSL, or HSV).</param>
+	public static void Command([Argument] string color, Form form) {
+		HexColor hexColor;
+
+		try {
+			hexColor = new(color);
+		} catch {
+			Console.Error.WriteLine("error: Color must be a 3-digit or 6-digit hexadecimal optionally preceeded by a '#'.");
+			return;
+		}
+
+		try {
+			switch (form) {
+				case Form.RGB:
+					Console.WriteLine(hexColor.ToRgbString());
+					break;
+				case Form.HSL:
+					Console.WriteLine(hexColor.ToHslString());
+					break;
+				case Form.HSV:
+					Console.WriteLine(hexColor.ToHsvString());
+					break;
+			}
+		} catch (Exception error) {
+			Console.Error.WriteLine($"error: {error.Message}");
+		}
+	}
+}
--- ColorConverter/Program.cs
+++ ColorConverter/Program.cs
@@ -1,47 +1,9 @@
 ﻿namespace ColorConverter;
 
+using ConsoleAppFramework;
+
 internal class Program {
-	static void Main() {
-		while (true) {
-			HexColor? color = null;
-			do {
-				string response = InputUtil.Prompt("Enter Hex Code: ");
-
-				if (response == "") {
-					Console.WriteLine("Exiting...");
-					Environment.Exit(0);
-				}
-
-				try {
-					color = new HexColor(response);
-				} catch {
-					Console.Error.WriteLine("error: Must be a 3-digit or 6-digit hexadecimal optionally preceeded by a '#'.");
-				}
-			} while (color == null);
-
-			Console.WriteLine($"Convert {color} to...");
-			Console.WriteLine("1: RGB");
-			Console.WriteLine("2: HSL");
-			Console.WriteLine("3: HSV");
-			string option = InputUtil.Prompt("Enter Choice: ", (response) => InputUtil.IsInRange(response, 1, 3), "Must select option 1, 2, or 3.");
-			int optionNum = Int32.Parse(option);
-
-			try {
-				switch (optionNum) {
-					case 1:
-						Console.WriteLine(color.ToRgbString());
-						break;
-					case 2:
-						Console.WriteLine(color.ToHslString());
-						break;
-					case 3:
-						Console.WriteLine(color.ToHsvString());
-						break;
-				}
-				Console.WriteLine();
-			} catch (Exception error) {
-				Console.Error.WriteLine($"error: {error.Message}");
-			}
-		}
+	static void Main(string[] args) {
+		ConsoleApp.Run(args, ConverterCommand.Command);
 	}
 }
